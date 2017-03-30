15bb6c9ec4fbcf2675d34bc6427a31cd97aa7051
joseramirez28 <jose_ramirez28@outlook.com>
2017-03-30 08:38:49 -0600

    change

---
+++ aop-aspectj/src/main/java/org/j4g/spring/aop/MainApp.java
@@ -0,0 +1,15 @@
+package org.j4g.spring.aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+
+		ApplicationContext appContext = new ClassPathXmlApplicationContext(new String[] { "Spring-Beans.xml" });
+
+		Object cust = (Object) appContext.getBean("ServiceProxy");
+
+	}
+}
